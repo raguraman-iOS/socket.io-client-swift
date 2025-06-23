a492be9d064909fd6b88b46906b5280c70d5a51e
Raguraman <raguraman.asokan@gmail.com>
2025-06-23 13:21:02 +0300

    Starscream version updated to 4.0.8

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SocketIO", targets: ["SocketIO"])
     ],
     dependencies: [
-        .package(url: "https://github.com/daltoniam/Starscream", .upToNextMinor(from: "3.1.0")),
+        .package(url: "https://github.com/daltoniam/Starscream", .upToNextMajor(from: "4.0.8")),
     ],
     targets: [
         .target(name: "SocketIO", dependencies: ["Starscream"]),
